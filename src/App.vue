4b20a1cf501fc412bc59ebb08a4ff005995b9c2b
Darcy <mr_darcy_1@mail.ru>
2025-02-15 10:57:08 +0400

    project start (#2)
    * feat: add Tailwind and its peer-dependencies using npm:
    
    * feat: generate tailwind.config.js and postcss.config.js files
    
    * feat: add fontFamily,
              gridTemplateColumns on tailwind.config.js
    
    * feat: include tailwind in css
    
    * feat: add theme-files
    
    * feat: add Navbar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,7 @@
 <script setup>
-import { onMounted, ref } from "vue";
-
-const name = ref('Arthur Morgan');
-const status = ref('active');
-const tasks = ref([]);
-const link = ref("https://google.com");
-const newTask = ref('');
-
-const toggleStatus = () => {
-  const statuses = ['active', 'pending', 'inactive'];
-  status.value = statuses[(statuses.indexOf(status.value) + 1) % statuses.length];
-};
-
-const addTask = () => {
-  if (!newTask.value.trim()) return;
-  tasks.value = [...tasks.value, newTask.value];
-  newTask.value = '';
-};
-
-const deleteTask = (index) => {
-  tasks.value = tasks.value.filter((_, i) => i !== index);
-};
-
-onMounted(async () => {
-  try {
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5');
-    const data = await res.json();
-    tasks.value = data.map(task => task.title);
-  } catch (error) {
-    console.error('Ошибка загрузки данных:', error);
-  }
-});
+import Navbar from "@/components/Navbar.vue";
 </script>
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status==='active'">User is active</p>
-  <p v-else-if="status==='pending'">User is pending</p>
-  <p v-else>User is inactive</p>
+  <Navbar/>
 
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <br>
-    <input type="text" id="newTask" name="newTask" v-model="newTask"/>
-    <button type="submit" @click="addTask">Submit</button>
-  </form>
-
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for="(task,index) in tasks" :key="task">
-      <span @key="index">
-        {{ task }}
-      </span>
-      <button @click="deleteTask(index)">
-        x
-      </button>
-    </li>
-  </ul>
-  <!--  <a v-bind:href="link">Click for Google</a>-->
-  <a :href="link">Click for Google</a>
-
-  <!--  <button v-on:click="toggleStatus">Change Status</button>-->
-  <button @click="toggleStatus">Change Status</button>
 </template>
