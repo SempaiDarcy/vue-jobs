e4eab3571bf0555ab545fc074478e579858867c2
SempaiDarcy <mr_darcy_1@mail.ru>
2025-02-15 11:14:23 +0400

    feat: add HomeCard.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Navbar from "@/components/Navbar/Navbar.vue";
 import Hero from "@/components/Hero/Hero.vue";
+import HomeCard from "@/components/HomeCard/HomeCard.vue";
 </script>
 <template>
   <Navbar/>
   <Hero title="Test title" subTitle="Test subtitle"/>
+  <HomeCard/>
 </template>
