37c6ba41fcd34d58d064d52bc6950d39a5eb3cfb
Darcy <mr_darcy_1@mail.ru>
2025-02-16 14:37:24 +0400

    components (#3)
    * feat: add Hero.vue
    
    * feat: add HomeCard.vue
    
    * feat: add Card.vue
    
    * feat: add JobListings.vue and jobs.json
    
    * feat: add computed property and function for truncated job description

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
-import Navbar from "@/components/Navbar.vue";
+import Navbar from "@/components/Navbar/Navbar.vue";
+import Hero from "@/components/Hero/Hero.vue";
+import HomeCard from "@/components/HomeCard/HomeCard.vue";
+import JobListings from "@/components/JobListings/JobListings.vue";
 </script>
 <template>
   <Navbar/>
-
+  <Hero title="Test title" subTitle="Test subtitle"/>
+  <HomeCard/>
+  <JobListings :limit="3" :show-button="true"/>
 </template>
