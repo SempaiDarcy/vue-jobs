b8422a23dbd46618419f79dcdbf4cd07b118c3c1
SempaiDarcy <mr_darcy_1@mail.ru>
2025-02-16 19:33:26 +0400

    feat: add router, HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Hero from "@/components/Hero/Hero.vue";
+import JobListings from "@/components/JobListings/JobListings.vue";
+import HomeCard from "@/components/HomeCard/HomeCard.vue";
+</script>
+<template>
+  <Hero/>
+  <HomeCard/>
+  <JobListings :limit="3" :show-button="true"/>
+</template>
