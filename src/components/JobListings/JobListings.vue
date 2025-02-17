fdf2a974a477d0914bd0638982062630689a1fab
Darcy <mr_darcy_1@mail.ru>
2025-02-17 16:52:37 +0400

    json server rest api (#5)
    * feat: add new script "server": "json-server --watch src/jobs.json --port 8000" and install "axios": "^1.7.9"
    
    * feat: add new script "server": "json-server --watch src/jobs.json --port 8000" and install "axios": "^1.7.9"
    
    * feat: replaced static job data with dynamic API fetching using axios
    
    * feat: add lib:"vue-spinner": "^1.0.4"
    
    * feat: added job loading via API, improved state management, and added loading spinner

--- src/components/JobListings/JobListings.vue
+++ src/components/JobListings/JobListings.vue
@@ -1,8 +1,12 @@
 <script setup>
-import jobData from '@/jobs.json'
-import {ref, defineProps} from 'vue'
+import {
+  // ref,
+  defineProps, onMounted, reactive
+} from 'vue'
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 import {RouterLink} from "vue-router";
 import JobListing from "@/components/JobListings/JobListing/JobListing.vue";
+import axios from "axios";
 
 defineProps({
   limit: Number,
@@ -11,8 +15,23 @@ defineProps({
     default: false
   }
 })
-const jobs = ref(jobData)
-console.log(jobData)
+
+// const jobs = ref([])
+const state = reactive({
+  jobs: [],
+  isLoading: true
+})
+onMounted(async () => {
+  try {
+    const res = await axios.get('http://localhost:8000/jobs')
+    state.jobs = res.data;
+  } catch (error) {
+    console.error('Error fetching jobs', error)
+  } finally {
+    state.isLoading = false
+  }
+})
+
 </script>
 <template>
   <section class="bg-blue-50 px-4 py-10">
@@ -20,8 +39,13 @@ console.log(jobData)
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs.slice(0, limit || jobs.length)"
+      <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader/>
+      </div>
+      <!-- Shoe job listing when done loading -->
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
                     :key="job.id"
                     :job="job"
         />
@@ -30,7 +54,7 @@ console.log(jobData)
   </section>
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
     <RouterLink to="/jobs"
-    class="block bg-black text-white text-center py-4 px-6 rounded hover:bg-gray-700"
+                class="block bg-black text-white text-center py-4 px-6 rounded hover:bg-gray-700"
     >
       View All jobs
     </RouterLink>
