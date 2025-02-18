c44ab0f566730ec686eceb905ef5ec9515cafd19
Darcy <mr_darcy_1@mail.ru>
2025-02-18 16:42:02 +0400

    job page (#6)
    * feat: add BackButton.vue
    
    * feat: added API proxy in Vite, refactored API calls
    
    * feat: add AddJobView.vue implemented v-model for form inputs to enable two-way data binding
    
    * fix: add pi-map-marker for tag <i> on JobView.vue
    
    * feat: added job submission via API and automatic redirection to job details page
    
    * feat: add lib: "vue-toastification": "^2.0.0-rc.5"
    
    * feat: added toast notifications for job submission success and error handling
    
    * feat: added job deletion with confirmation and toast notifications
    
    * feat: fixed edit job route and improved RouterLink navigation
    
    * feat: added job update functionality with form pre-filling and API integration

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,10 +1,16 @@
 <script setup>
-import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import {reactive, onMounted} from "vue";
-import {useRoute, RouterLink} from "vue-router";
+import {useRoute, RouterLink, useRouter} from "vue-router";
 import axios from "axios";
 
+import {useToast} from "vue-toastification";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+import BackButton from "@/components/BackButton/BackButton.vue";
+
+
 const route = useRoute()
+const router = useRouter()
+const toast = useToast()
 
 const jobId = route.params.id;
 
@@ -12,9 +18,24 @@ const state = reactive({
   job: {},
   isLoading: true
 })
+
+const deleteJob = async () => {
+  try {
+    const confirm = window.confirm('Are you sure you want to delete this job?')
+    if(confirm) {
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success('Job Deleted Successfully');
+      router.push('/jobs')
+    }
+  } catch (e) {
+    console.error('Error deleting job', e)
+    toast.error('Job Not Deleted')
+  }
+}
+
 onMounted(async () => {
   try {
-    const res = await axios.get(`http://localhost:8000/jobs/${jobId}`)
+    const res = await axios.get(`/api/jobs/${jobId}`)
     state.job = res.data;
     console.log(res.data)
   } catch (error) {
@@ -26,6 +47,7 @@ onMounted(async () => {
 console.log(state.job)
 </script>
 <template>
+  <BackButton/>
   <section v-if="!state.isLoading" class="bg-green-50">
     <div class="container m-auto py-10 px-6">
       <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
@@ -39,7 +61,7 @@ console.log(state.job)
                 class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
             >
               <i
-                  class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
+                  class="pi pi-map-marker text-xl text-orange-700 mr-2"
               ></i>
               <p class="text-orange-700">{{ state.job.location }}</p>
             </div>
@@ -91,12 +113,13 @@ console.log(state.job)
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-xl font-bold mb-6">Manage Job</h3>
             <RouterLink
-                to="`/jobs/edit/${state.job.id}`"
+                :to="`/jobs/edit/${state.job.id}`"
                 class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >Edit Job
             </RouterLink
             >
             <button
+                @click="deleteJob"
                 class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
               Delete Job
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +12,13 @@ export default defineConfig({
   ],
   "server": {
     port:3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   resolve: {
     alias: {
